6252892965340dc2b98809c9931eeddc881de319
sabenitez-unal <sabenitez@unal.edu.co>
2026-02-20 16:20:02 -0500

    main configurado para pruebas

--- src/main.py
+++ src/main.py
@@ -1,5 +1,13 @@
+import asyncio
+from translate import translate_text as translate
+
 def main():
-    pass
+    params = dict()
+
+    params["text"] = input()
+ 
+    texto = asyncio.run(translate(params))
+    print(texto)
 
 if __name__ == "__main__":
     main()
